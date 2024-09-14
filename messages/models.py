14e31dab645cc392ba99e32dc74ace67470603f4
zedsalim <salimzaiidi@gmail.com>
2024-09-14 09:34:52 +0100

    Moving Message & Reply models to the messages app

--- messages/models.py
+++ messages/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from users.models import UserProfile
 
-# Create your models here.
+
+class Message(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Replied', 'Replied'),
+        ('Spam', 'Spam'),
+    )
+    sender = models.ForeignKey(UserProfile, null=True, on_delete=models.SET_NULL)
+    subject = models.CharField(max_length=100)
+    content = models.TextField()
+    email = models.EmailField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS, default='Pending')
+    
+    def __str__(self):
+        return f'Subject: {self.subject}'
+
+class Reply(models.Model):
+    responder = models.ForeignKey(UserProfile, null=True, on_delete=models.SET_NULL)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    content = models.TextField()
+    replied_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Reply'
+        verbose_name_plural = 'Replies'
+    
+    def __str__(self):
+        return f'Reply To: {self.message.subject}'
--- users/models.py
+++ users/models.py
@@ -78,33 +78,3 @@ class UserProfile(models.Model):
     def __str__(self):
         full_name = f'{self.user.first_name} {self.user.last_name}'
         return full_name.title()
-
-
-class Message(models.Model):
-    STATUS = (
-        ('Pending', 'Pending'),
-        ('Replied', 'Replied'),
-        ('Spam', 'Spam'),
-    )
-    sender = models.ForeignKey(UserProfile, null=True, on_delete=models.SET_NULL)
-    subject = models.CharField(max_length=100)
-    content = models.TextField()
-    email = models.EmailField()
-    sent_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=20, choices=STATUS, default='Pending')
-    
-    def __str__(self):
-        return f'Subject: {self.subject}'
-
-class Reply(models.Model):
-    responder = models.ForeignKey(UserProfile, null=True, on_delete=models.SET_NULL)
-    message = models.ForeignKey(Message, on_delete=models.CASCADE)
-    content = models.TextField()
-    replied_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        verbose_name = 'Reply'
-        verbose_name_plural = 'Replies'
-    
-    def __str__(self):
-        return f'Reply To: {self.message.subject}'
